68d58f05d0db2e4d6a4fbfd207b817af9acae3d4
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-02 12:02:22 +0530

    modified

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 require('dotenv').config();
+require('./config/database').connect();
 const express = require('express');
 
+const User = require('./model/user')
+
 const app = express();
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('Hello from auth system');
 });
 
+app.post('/register', async (req,res) => {
+
+    const { firstname ,lastname ,email ,password }  = req.body;
+
+    if (!(firstname && lastname && email && password)) {
+        
+        res.status(401).send("All Fields Required")
+    }
+
+    const existingUser = await User.findOne({ email });
+    
+    if (existingUser) {
+        res.status(401).send("User Already Exists");
+
+    }
+});
 
 module.exports = app;
