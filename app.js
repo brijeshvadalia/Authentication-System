8b6d3a3e5f1258aa0ba01e083cc79c6be380ced2
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-02 14:55:11 +0530

    Modified

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const User = require('./model/user')
 
@@ -13,6 +15,7 @@ app.get('/',(req,res)=>{
 
 app.post('/register', async (req,res) => {
 
+  try {
     const { firstname ,lastname ,email ,password }  = req.body;
 
     if (!(firstname && lastname && email && password)) {
@@ -26,6 +29,61 @@ app.post('/register', async (req,res) => {
         res.status(401).send("User Already Exists");
 
     }
+
+    const encryptedPassword = await bcrypt.hash(password,10)
+
+    const user = User.create({
+        firstname,
+        lastname,
+        email:email.toLowerCase(),
+        password : encryptedPassword
+    })
+
+    const token = jwt.sign(
+        { user_id  : user._id,email},
+        process.env.SECRET_KEY,
+        {
+            expiresIn : "5h"
+        }
+
+    );
+    user.token = token;
+    user.password = undefined;
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+app.post('/login', async (req,res) => {
+
+    try {
+        const { email , password }  = req.body;
+
+        if (!(email && password)) {
+          res.status(401).send("Field is missing")
+        }
+
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password,user.password))){
+
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.SECRET_KEY,
+                {
+                  expiresIn: "5h",
+                }
+              )
+              user.token = token;
+              user.password = undefined;
+              res.status(201).json(user);
+            }
+           res.status(401).send('Email or Password is Incorrect');
+
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 module.exports = app;
